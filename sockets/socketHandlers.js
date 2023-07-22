41ea3ed86967e9b99ff9c7b94943cd7e20e9aad1
bigmosi <kinyeramo@gmail.com>
2023-07-22 17:45:04 +0300

    fixed issues

--- sockets/socketHandlers.js
+++ sockets/socketHandlers.js
@@ -6,16 +6,13 @@ const handleSocketEvents = (server) => {
     io.on('connection', (socket) => {
         console.log('User connected:', socket.id);
 
-        // Join a room (note) when requested by the client
         socket.on('joinRoom', (room) => {
             console.log('User joining room:', room);
             socket.join(room);
         });
 
-        // Listen for note updates from the client
         socket.on('updateNote', (room, updatedNote) => {
             console.log('Note updated in room:', room);
-            // Emit the updated note to all clients in the room
             io.to(room).emit('noteUpdated', updatedNote);
         });
 
